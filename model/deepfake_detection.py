bc7ac7f111ed71ddb6e2200852514386fd4c11df
Urvashi <urvashirichaagrawal@gmail.com>
2024-09-01 12:27:19 +0530

    back btn added

--- model/deepfake_detection.py
+++ model/deepfake_detection.py
@@ -1,25 +1,36 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
+import os
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes and origins
+CORS(app)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/detect', methods=['POST'])
 def detect():
-    data = request.get_json()
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
 
-    if 'image_data' not in data:
-        return jsonify({'error': 'No image data provided'}), 400
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
 
-    image_data = data['image_data']
-    # Process the image data and make predictions here
+    if file:
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
 
-    # Simulate response
-    response = {
-        'label': 'Deepfake' if random.random() < 0.5 else 'Not a Deepfake'
-    }
-    return jsonify(response)
+        # Process the image here
+        # Simulate response
+        response = {
+            'label': 'Deepfake' if random.random() < 0.5 else 'Not a Deepfake'
+        }
+        return jsonify(response)
 
 if __name__ == '__main__':
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
     app.run(port=5000)
